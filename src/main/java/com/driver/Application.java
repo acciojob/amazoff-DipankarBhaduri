587ab50a093836667d801894345bab19f1eef9b4
Dipankar Bhaduri <110708060+DipankarBhaduri@users.noreply.github.com>
2023-02-11 11:56:31 +0530

    Created Service and repository class & Implemented methods

--- src/main/java/com/driver/Application.java
+++ src/main/java/com/driver/Application.java
@@ -1,5 +1,4 @@
 package com.driver;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -7,9 +6,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-
 	}
 }
